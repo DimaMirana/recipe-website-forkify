a7aebc61398404ad2418c84b60776d740cbffd5e
Tasnim Ferdous Dima <48598224+DimaMirana@users.noreply.github.com>
2019-10-15 04:05:11 +0600

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+/*jshint esversion: 8 */
+//b55e6a5604ef8f77051f3f59038ca9ab -food2fork api key
+//https://crossorigin.me/ -proxy1
+//https://cors-anywhere.herokuapp.com/
+//https://www.food2fork.com/api/search -for search foods
+
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = 'b55e6a5604ef8f77051f3f59038ca9ab';
---
+++ src/js/models/List.js
@@ -0,0 +1,30 @@
+/*jshint esversion: 8 */
+import uniqid from 'uniqid';
+
+export default class List {
+  constructor() {
+    this.items = [];
+  }
+
+  addItem (count, unit, ingredient) {
+    const item = {
+      id : uniqid(),
+      count,
+      unit,
+      ingredient
+    };
+    this.items.push(item);
+    return item;
+  }
+
+  deleteItem (id) {
+    const index = this.items.findIndex(el => el.id ===id);
+    //[2, 4, 8] splice[1, 1//how many elements we want to take] -->return 4 original array is [2,8]
+    //[2, 4, 8] slice[1, 1//upto how many element it will take updo this index] -->return 4 original array is [2,4,8]
+    this.items.splice(index,1);
+  }
+
+  updateCount(id, newCount) {
+    this.items.find(el => el.id === id).count = newCount;
+  }
+}
